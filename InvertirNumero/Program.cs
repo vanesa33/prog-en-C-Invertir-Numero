25547160fbce26f9ad8dbe26e60c89149c6e359f
Vanidad <57409926+vanesa33@users.noreply.github.com>
2021-02-10 16:35:25 -0300

    INVERTIR NÚMERO DE DOS CIFRAS
    Invertir número de dos cifras con operaciones aritméticas

---
+++ InvertirNumero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUM, AUX, DEC, UNI;
+            string linea;
+            Console.WriteLine("INGRESE NÚMERO DE DOS CIFRAS :");
+            linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+            DEC = NUM / 10;
+            UNI = NUM % 10;
+            AUX = (UNI * 10) + DEC;
+            Console.WriteLine("NÚMERO INVERTIDO ES: " + AUX);
+            Console.WriteLine("Pulse una Tecla:"); Console.ReadLine();
+        }
+    }
+}
